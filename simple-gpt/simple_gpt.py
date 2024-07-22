119027803f6185f60942a3a8e9c21d70da687dae
Daniel Dager <danieldager@Daniels-MacBook-Pro-3.local>
2024-07-22 11:28:24 +0200

    simple-gpt runs, saves weights

--- simple-gpt/simple_gpt.py
+++ simple-gpt/simple_gpt.py
@@ -5,11 +5,11 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 64 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
-max_iters = 5000
-eval_interval = 500
+max_iters = 500
+eval_interval = 50
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
+eval_iters = 20
 n_embd = 384
 n_head = 6
 n_layer = 6
@@ -29,7 +29,7 @@ vocab_size = len(chars)
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+encode = lambda s: [stoi[c] for c in s]          # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
 # Train and test splits
@@ -47,21 +47,6 @@ def get_batch(split):
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     x, y = x.to(device), y.to(device)
     return x, y
-
-@torch.no_grad()
-def estimate_loss():
-    out = {}
-    model.eval()
-    for split in ['train', 'val']:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            X, Y = get_batch(split)
-            logits, loss = model(X, Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
-
 
 class Head(nn.Module):
     """ one head of self-attention """
@@ -94,3 +79,190 @@ class Head(nn.Module):
         return out
 
 
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([
+            Head(head_size) for _ in range(num_heads)
+        ])
+        
+        # instantiate a projection back into the residual pathway
+        self.proj = nn.Linear(n_embd, n_embd)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
+    
+
+class FeedForward(nn.Module):
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(*[
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd) # projection into residual pathway
+        ])
+
+    def forward(self, x):
+        return self.net(x)
+    
+
+class Block(nn.Module):
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        # important !! x + ... implements residual connections !
+        # this allows for smooth transitions in the flow of gradients !
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x 
+
+
+class GPT(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        # token and positional embeddings
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        
+        # multiheaded perception blocks w/ feedforward networks + layer norms
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head) for _ in range(n_layer)])
+
+        # final layer norm
+        self.ln_f = nn.LayerNorm(n_embd, n_embd)
+
+        # reprojection into token
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+        # initialize all weights recursively (i.e. what apply does)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        
+    def forward(self, idx, targets=None):
+        # idx and targets are both (B, T) tensors
+        B, T = idx.shape
+
+        # token and position embedding            # (B, T, C), or otherwise seen as
+        tok_emb = self.token_embedding_table(idx) # (batch_size, block_size, n_embd)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
+        x = tok_emb + pos_emb # (B, T, C) again 
+
+        # passing embedded tokens through the model
+        x = self.blocks(x)
+        x = self.ln_f(x)
+
+        # outputs the probabilities of possible next tokens
+        logits = self.lm_head(x) # (B, T, vocab_size)
+
+        if targets is None:
+            loss = None
+        
+        # calculate the loss for generating gradients
+        # reshape tensors to conform to lost function
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C) 
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        # idx is a (B, T) array of indicies in the current context 
+
+        for _ in range(max_new_tokens):
+
+            # crop "prompt" (tokens) to block size
+            idx_cond = idx[:, -block_size:]
+
+            # forward pass to get the logits
+            logits, _ = self(idx_cond) # self == self.forward for nn.Module
+
+            # extract last time step
+            logits = logits[:, -1, :] # (B, C)
+
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+
+        return idx
+
+model = GPT()
+m = model.to(device)
+
+# print the number of parameters in the model
+print(sum(p.numel() for p in m.parameters()) / 1e6, "M parameters")
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            _, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+# load model weights, if they exist
+try:
+    model.load_state_dict(torch.load('weights/model_weights.pth'))
+except:
+
+    """ Training Loop """
+
+    for iter in range(max_iters):
+
+        # regularly evaluate loss on the validation dataset
+        if iter % eval_interval == 0 or iter == max_iters:
+            losses = estimate_loss()
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        
+        # sample a batch
+        xb, yb = get_batch('train')
+
+        # evaluate the loss
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True) # set_to_none increases efficiency
+        loss.backward()
+        optimizer.step()
+
+# save the model's weights
+torch.save(model.state_dict(), 'weights/model_weights.pth')
+
+
+""" Generate from the Model """
+
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
